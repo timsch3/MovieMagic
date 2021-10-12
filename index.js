1dee4a65125fef6d30ea6fcc074313c307fdfa3d
timsch3 <timsch3@gmail.com>
2021-10-12 16:47:41 +0200

    first api call working

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const ejs = require('ejs');
 const express = require('express')
 const app = express()
+const axios = require('axios').default
+
 let activePage = 1
 let moviesPerPage = 10
 // placeholder objects
-let movies = require('./data')
+// let movies = require('./data')
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('pages/index', { movies: movies, activePage: activePage, moviesPerPage: moviesPerPage })
+    axios.get('https://api.themoviedb.org/3/discover/movie?api_key=3dec7146ea3697eb18b4879a213a0de1')
+    .then(function (response) {
+        res.render('pages/index', { movies: response.data.results, activePage: activePage, moviesPerPage: moviesPerPage })
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
 })
 
 app.get('/:page', (req, res) => {
