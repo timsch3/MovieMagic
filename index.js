502c96f7587d9227087d70f44a9587184e89c542
timsch3 <timsch3@gmail.com>
2021-10-12 16:06:10 +0200

    added moviesPerPage var and fixed loop for showing movies

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const ejs = require('ejs');
 const express = require('express')
 const app = express()
-let activePage
+let activePage = 1
+let moviesPerPage = 10
 // placeholder objects
 let movies = require('./data')
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('pages/index', { movies: movies, activePage: Number(0) })
+    res.render('pages/index', { movies: movies, activePage: activePage, moviesPerPage: moviesPerPage })
 })
 
 app.get('/:page', (req, res) => {
-    res.render('pages/index', { movies: movies, activePage: Number(req.params.page) })
+    res.render('pages/index', { movies: movies, activePage: Number(req.params.page), moviesPerPage: moviesPerPage })
 })
 
 app.listen(3000, () => {
