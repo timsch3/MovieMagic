daf60d33600c4251f4e10ce9caf8d70c7d029720
timsch3 <timsch3@gmail.com>
2021-10-13 20:55:19 +0200

    navigation functional, created details page, all api calls, first styling

--- index.js
+++ index.js
@@ -3,25 +3,38 @@ const express = require('express')
 const app = express()
 const axios = require('axios').default
 
-let activePage = 1
-let moviesPerPage = 10
-// placeholder objects
-// let movies = require('./data')
-
 app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     axios.get('https://api.themoviedb.org/3/discover/movie?api_key=3dec7146ea3697eb18b4879a213a0de1')
     .then(function (response) {
-        res.render('pages/index', { movies: response.data.results, activePage: activePage, moviesPerPage: moviesPerPage })
+        res.render('pages/index', { movies: response.data.results, activePage: 1 })
     })
     .catch(function (error) {
         console.log(error);
     })
 })
 
-app.get('/:page', (req, res) => {
-    res.render('pages/index', { movies: movies, activePage: Number(req.params.page), moviesPerPage: moviesPerPage })
+app.get('/details/:id', (req, res) => {
+    axios.get(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=3dec7146ea3697eb18b4879a213a0de1`)
+    .then(function (response) {
+        res.render('pages/details', { movie: response.data })
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+})
+
+app.get('/page/:page', (req, res) => {
+    axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=3dec7146ea3697eb18b4879a213a0de1&page=${req.params.page}`)
+    .then(function (response) {
+        res.render('pages/index', { movies: response.data.results, activePage: Number(req.params.page) })
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
 })
 
 app.listen(3000, () => {
