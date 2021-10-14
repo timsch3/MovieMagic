0251d6c901eb3efd50aa3b858a121f46ce8d2072
timsch3 <timsch3@gmail.com>
2021-10-14 12:51:05 +0200

    more styling, extended page nav

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     axios.get('https://api.themoviedb.org/3/discover/movie?api_key=3dec7146ea3697eb18b4879a213a0de1')
     .then(function (response) {
-        res.render('pages/index', { movies: response.data.results, activePage: 1 })
+        res.render('pages/index', { movies: response.data, activePage: 1 })
     })
     .catch(function (error) {
         console.log(error);
@@ -30,7 +30,7 @@ app.get('/details/:id', (req, res) => {
 app.get('/page/:page', (req, res) => {
     axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=3dec7146ea3697eb18b4879a213a0de1&page=${req.params.page}`)
     .then(function (response) {
-        res.render('pages/index', { movies: response.data.results, activePage: Number(req.params.page) })
+        res.render('pages/index', { movies: response.data, activePage: Number(req.params.page) })
     })
     .catch(function (error) {
         console.log(error);
