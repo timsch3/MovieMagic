6a22c49f38c96ee259e7ba0e093b8eb3994359b4
timsch3 <timsch3@gmail.com>
2021-10-15 11:46:16 +0200

    added genre selection incl pagination

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const ejs = require('ejs');
+const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express()
 const axios = require('axios').default
@@ -8,19 +9,9 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    axios.get('https://api.themoviedb.org/3/discover/movie?api_key=3dec7146ea3697eb18b4879a213a0de1&include_adult=false')
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
     .then(function (response) {
-        res.render('pages/index', { movies: response.data, activePage: 1 })
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-})
-
-app.get('/details/:id', (req, res) => {
-    axios.get(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=3dec7146ea3697eb18b4879a213a0de1`)
-    .then(function (response) {
-        res.render('pages/details', { movie: response.data })
+        res.render('pages/index', { movies: response.data, activePage: 1, genreChosen: false })
     })
     .catch(function (error) {
         console.log(error);
@@ -28,9 +19,39 @@ app.get('/details/:id', (req, res) => {
 })
 
 app.get('/page/:page', (req, res) => {
-    axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=3dec7146ea3697eb18b4879a213a0de1&include_adult=false&page=${req.params.page}`)
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&page=${req.params.page}`)
     .then(function (response) {
-        res.render('pages/index', { movies: response.data, activePage: Number(req.params.page) })
+        res.render('pages/index', { movies: response.data, activePage: Number(req.params.page), genreChosen: false })
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+})
+
+app.get('/genre/:id/page/:page', (req, res) => {
+    axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&with_genres=${req.params.id}&page=${req.params.page}`)
+    .then(function (response) {
+        res.render('pages/index', { movies: response.data, activePage: Number(req.params.page), genreChosen: true, genreID: Number(req.params.id) })
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+})
+
+// app.get('/search/:term', (req, res) => {
+//     axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&page=${req.params.page}`)
+//     .then(function (response) {
+//         res.render('pages/index', { movies: response.data, activePage: Number(req.params.page) })
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     })
+// })
+
+app.get('/details/:id', (req, res) => {
+    axios.get(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.API_KEY}`)
+    .then(function (response) {
+        res.render('pages/details', { movie: response.data })
     })
     .catch(function (error) {
         console.log(error);
