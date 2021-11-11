a54f6ac3c1fb31f71f25275c082038b374fb832f
unknown <timsch3@gmail.com>
2021-11-11 21:30:05 +0100

    implemented active genre being highlighted

--- index.js
+++ index.js
@@ -8,35 +8,48 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
+let genreList = [] // Get a list of all genres
+let gotGenreList = false
+if (!gotGenreList) {
+    axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.API_KEY}`)
+        .then(function (response) {
+            genreList = response.data.genres
+            gotGenreList = true
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
 // HOME
 app.get('/', (req, res) => {
     axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
-    .then(function (response) {
-        res.render('pages/index', { movies: response.data, activePage: 1, genreChosen: false, searchTermEntered: false, searchTerm: '' })
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
+        .then(function (response) {
+            res.render('pages/index', { genreList, movies: response.data, activePage: 1, genreChosen: false, searchTermEntered: false, searchTerm: '' })
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
 })
 // PAGES
 app.get('/page/:page', (req, res) => {
     axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&page=${req.params.page}`)
-    .then(function (response) {
-        res.render('pages/index', { movies: response.data, activePage: Number(req.params.page), genreChosen: false, searchTermEntered: false, searchTerm: '' })
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
+        .then(function (response) {
+            res.render('pages/index', { genreList, movies: response.data, activePage: Number(req.params.page), genreChosen: false, searchTermEntered: false, searchTerm: '' })
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
 })
 // GENRES
 app.get('/genre/:id/page/:page', (req, res) => {
     axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&with_genres=${req.params.id}&page=${req.params.page}`)
-    .then(function (response) {
-        res.render('pages/index', { movies: response.data, activePage: Number(req.params.page), genreChosen: true, genreID: Number(req.params.id), searchTermEntered: false, searchTerm: '' })
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
+        .then(function (response) {
+            res.render('pages/index', { genreList, movies: response.data, activePage: Number(req.params.page), genreChosen: true, genreID: Number(req.params.id), searchTermEntered: false, searchTerm: '' })
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
 })
 // SEARCH
 app.get('/search?:term', (req, res) => {
@@ -45,24 +58,24 @@ app.get('/search?:term', (req, res) => {
 })
 app.get('/search/:term/page/:page', (req, res) => {
     axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${req.params.term}&page=${req.params.page}`)
-    .then(function (response) {
-        res.render('pages/index', { movies: response.data, activePage: Number(req.params.page), genreChosen: false, searchTermEntered: true, searchTerm: req.params.term })
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
+        .then(function (response) {
+            res.render('pages/index', { genreList, movies: response.data, activePage: Number(req.params.page), genreChosen: false, searchTermEntered: true, searchTerm: req.params.term })
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
 })
 // DETAILS PAGE
 app.get('/details/:id', (req, res) => {
     axios.get(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.API_KEY}`)
-    .then(function (response) {
-        res.render('pages/details', { movie: response.data, searchTerm: '' })
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
+        .then(function (response) {
+            res.render('pages/details', { movie: response.data, searchTerm: '' })
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
 })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('server running')
+    console.log('server running on http://localhost:3000')
 })
